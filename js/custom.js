620775b0c54e2fa5c3068c523c5500a67a83894d
Enrisan2705 <119128930+Enrisan2705@users.noreply.github.com>
2024-04-16 15:37:32 +0200

    Cambio info Enrique

--- js/custom.js
+++ js/custom.js
@@ -33,12 +33,12 @@ function show(member) {
         <div class="info_box">
             <img src="images/info_pers.png">
             <h3>Información Personal</h3>
-            <p>Estudiante de 4º de ingeniería informática interesado en el proceso de diseño y desarrollo de apps Android y en las Information Technologies (IT). Experiencia en el desarrollo de software, tanto aplicaciones web (fullstack) como de escritorio.</p>
+            <p>Estudiante de 4º de ingeniería informática interesado en el desarrollo de web-apps y en las bases de datos. Experiencia en el desarrollo de software, mayoritariamente en back-end, y en gestió básica de bases de datos. </p>
         </div>
         <div class="info_box">
             <img src="images/info_tfg.png">
             <h3>Trabajo de Fin de Grado (TFG)</h3>
-            <p>Actualmente trabajando en el desarrollo de una app para android que usa Inteligencia Artificial para rehabilitar pacientes con problemas cervicales mediante videojuegos en 2D.</p>
+            <p>Actualmente trabajando en una investigación sobre las posibles aplicaciones de la inteligencia artificial en el entorno de las Islas Baleares.</p>
         </div>
         <div class="info_box">
             <img src="images/info_apt.png">
