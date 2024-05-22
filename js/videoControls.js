c7884ed21b54d6b89fc8cc964fe1bd2fc9a2b9e6
mventura <mventuraburgos@gmail.com>
2024-05-23 00:41:50 +0200

    Reproductor

--- js/videoControls.js
+++ js/videoControls.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function(){
     var progressBar = document.querySelector(".progress-bar");
     var currentTime = document.querySelector(".current-time");
     var endTime = document.querySelector(".end-time");
+    
+    setupVolumeControl(),
 
     videoCine.addEventListener('loadedmetadata', function() {
         // Obtener la duración del video en segundos
@@ -99,3 +101,45 @@ function clickPartVideo(vid, bar){
         vid.currentTime = (e.offsetX / timelineWidth) * vid.duration;
     });
 }
+
+function setupVolumeControl() {
+    var audio = document.getElementById("video-cines")
+    var volumeSlider = document.getElementById("volume-slider")
+    var volumeIcon = document.getElementById("volume-icon");
+    audio.volume = .5;
+    volumeSlider.value = .5;
+
+    updateVolumeIcon(audio.volume, volumeIcon);
+    updateBarVolume(audio.volume, volumeSlider);
+
+    volumeSlider.addEventListener("input", function() {
+        const volume = volumeSlider.value;
+        audio.volume = volume;
+        updateVolumeIcon(volume, volumeIcon);
+        updateBarVolume(volume, volumeSlider);
+    });
+
+    volumeIcon.addEventListener("click", function() {
+        volumeSlider.value = 0;
+        audio.volume = 0;
+        updateVolumeIcon(0, volumeIcon);
+        updateBarVolume(0, volumeSlider);
+    });
+}
+
+function updateVolumeIcon(volume, volumeIcon) {
+    volumeIcon.className = volume <= 0.05 ? "fas fa-volume-mute" : volume > 0.05 && volume <= 0.5 ? "fas fa-volume-down" : "fas fa-volume-up";
+}
+
+function updateBarVolume(volume, volumeSlider) {
+    volumeSlider.style.background = `linear-gradient(to right, red 0%, red ${volume * 100}%, #ddd ${volume * 100}%, #ddd 100%)`;
+}
+
+// function updateVolumeIcon(volume, volumeIcon) {
+//     volumeIcon.className = volume <= .05 ? "fas fa-volume-mute" : volume > .05 && volume <= .5 ? "fas fa-volume-down" : "fas fa-volume-up"
+// }
+
+// function updateBarVolume(volume, volumeSlider) {
+//     const color = 0 == volume ? "#aaa" : `rgb(${255 * (1 - volume)}, ${255 * volume}, 0)`;
+//     volumeSlider.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${100 * volume}%, #fff ${100 * volume}%, #fff 100%)`
+// }
